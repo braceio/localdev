c7ede92ba976122a78a4d94645b74c27c99e0c9c
Cole Krumbholz <cole@colevscode.io>
2014-04-27 17:51:21 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ root = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name='localdev',
-    version='0.1.3',
+    version='0.1.4',
     author='Cole Krumbholz',
     author_email='cole@brace.io',
     description='Turn-key DNS server and proxy for multi-domain development.',
